47267c294d6f8493243194ba5150d92d180107ea
Paul <pavelnahratov8250@gmail.com>
2025-06-11 10:32:16 +0300

    __init__ edit

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 from .video_types import Image
 from .src.buffer.video_buffer import VideoBuffer, VideoBufferManager
 from .src.video.frame_reader import FrameReader, VideoCycleFrameReader, RTSPFrameReader
-from .src.utils.dump import dump2file, dump2file_delayed, save_delayed_init, save_delayed_autoaprove
+from .src.utils.dump import dump2file, dump2file_delayed, save_delayed_init, save_delayed_autoaprove, save_delayed_autoaprove_threaded, save_delayed_init_threaded
 from .src.utils.approval import DumpApproval
 
 __all__ = [
--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,4 +1,4 @@
-from .dump import dump2file, dump2file_delayed, save_delayed_init, save_delayed_autoaprove
+from .dump import dump2file, dump2file_delayed, save_delayed_init, save_delayed_autoaprove, save_delayed_init_threaded, save_delayed_autoaprove_threaded
 from .approval import DumpApproval
 
 __all__ = ["dump2file", "dump2file_delayed", "DumpApproval", "save_delayed_init", "save_delayed_autoaprove"]
